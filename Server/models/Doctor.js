3d9a9c4913bd7de33a55d9531a225d7e36036474
Kartik malik <sanjaykartik2222@gmail.com>
2024-04-30 20:55:38 +0530

    timestamps added to modeks

--- Server/models/Doctor.js
+++ Server/models/Doctor.js
@@ -40,7 +40,7 @@ const DoctorSchema = new mongoose.Schema({
         type:String,
         required:true
     }
-});
+},{timestamps:true});
 
 const DoctorModel = mongoose.model('Doctor', DoctorSchema);
 
