fe324ba2e2bbd35a18ec420247ed110a73774aa0
Kartik malik <sanjaykartik2222@gmail.com>
2024-04-30 21:54:28 +0530

    timestamps added to modeks

--- Server/models/User.js
+++ Server/models/User.js
@@ -17,7 +17,7 @@ const UserSchema = new mongoose.Schema({
         type:String,
         required:true,
     },
-});
+},{timestamps:true});
 
 const UserModel = mongoose.model('User', UserSchema);
 
